2199afdd996156d2aa0687c8ff9058ddb689bd9a
Jdtacke <joshuatackett.realtor@gmail.com>
2024-11-23 07:47:19 -0500

    JS additions 7:47am

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('Hello!');
+
+const subscribeButton = document.getElementById('subscribeButton');
+
+  // Add an event listener for the click event
+  subscribeButton.addEventListener('click', function() {
+    // Get the email input value
+    const email = document.getElementById('email').value;
+
+    // Show an alert with a message
+    if (email) {
+      alert(`Thank you for subscribing! We'll send updates to ${email}.`);
+    } else {
+      alert('Please enter a valid email address.');
+    }
+  });
+
+  
+ // "Add to Cart" button for Group Kickboxing
+ const gallery_add_to_cart = document.getElementById('gallery_add_to_cart');
+    gallery_add_to_cart.addEventListener('click', function() {
+        alert('Group Kickboxing has been added to your cart!');
+ });
+
+// "Add to Cart" button for Group Pilates
+const addToCartButton = document.getElementById('gallery');
+
+addToCartButton.addEventListener('click', function() {
+  alert('Group Pilates has been added to your cart!');
+});
+
+
+
+
